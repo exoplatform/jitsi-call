c645c2879e0891d9419dac7811a56dfce955cdc8
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-09-23 17:10:30 +0300

    Created Jitsi Call App microservice

---
+++ src/main/frontend/vue-app/components/Call.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="meet">
+   
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name : "Call",
+  data () {
+    return {
+      title: "eXo Jitsi Call",
+      backendResponse: [],
+      errors: []
+    };
+  }
+};
+</script>
