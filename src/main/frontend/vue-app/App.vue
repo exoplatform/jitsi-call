5518caa4b5d1c54a61c6c8c0533790ac945cb4d8
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-09-23 17:10:30 +0300

    Created Jitsi Call App microservice

---
+++ src/main/frontend/vue-app/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Call />
+  </div>
+</template>
+
+<script>
+import Call from "./components/Call.vue";
+
+export default {
+  name : "App",
+  components : {
+    Call
+  }
+};
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
