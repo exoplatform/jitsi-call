fff8fd57ce57209db01066eec687fb324c163ada
Vlad Martyshevskyi <vladmartishevskii@gmail.com>
2020-10-16 18:13:45 +0300

    Used env variables in finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -2,11 +2,10 @@
 
 RECORDINGS_DIR=$1
 # Put this script to .jitsi-meet-cfg/jibri (config folder) as finalize.sh
-# Need to setup $CALL_APP_URL as ENV variable before running
-
+# Need to setup $CALL_APP_URL and $EXTERNAL_JWT_SECRET as ENV variable before running
+echo "[debug] CALL APP URL: $CALL_APP_URL" >> finalize.log
+echo "[debug] EXO_JWT_SECRET: $EXO_JWT_SECRET" >> finalize.log
 #Build JWT 
-# TODO: store JWT in env variable
-secret='mQzPudDBpSAqUwM0FY2r86gNAd6be5tN1xqwdFDOb4Us1DT4Tx'
 currentTime=$(date +%s)
 # expires in 3 minutes
 expires=$(($currentTime + 18000));
@@ -17,12 +16,11 @@ payloadJson="{\"action\":\"external_auth\", \"exp\": ${expires}}"
 # Construct the payload
 payload=$(echo -n $payloadJson | base64 | sed s/\+/-/g |sed 's/\//_/g' |  sed -E s/=+$//)
 # Convert secret to hex (not base64)
-hexsecret=$(echo -n "$secret" | xxd -p | paste -sd "")
+hexsecret=$(echo -n "$EXO_JWT_SECRET" | xxd -p | paste -sd "")
 # Calculate hmac signature -- note option to pass in the key as hex bytes
 hmac_signature=$(echo -n "${jwt_header}.${payload}" |  openssl dgst -sha256 -mac HMAC -macopt hexkey:$hexsecret -binary | base64  | sed s/\+/-/g | sed 's/\//_/g' | sed -E s/=+$//)
 # Create the full token
 jwt="${jwt_header}.${payload}.${hmac_signature}"
-
 
 files=( $RECORDINGS_DIR/*.mp4 )
 recording=$(basename ${files[0]})
@@ -32,12 +30,11 @@ callId=${recording%_*}
 link=$(curl -o --write-out ' %{http_code}' --silent -k -X  GET -H "X-Exoplatform-External-Auth: ${jwt}" $CALL_APP_URL/api/v1/calls/${callId}/uploadLink)
 timestamp=$(date +%F_%T)
 if [ -n "$link" ]; then
-	echo $link
 	resp=$(curl -i --write-out '%{http_code}' --silent --output /dev/null -k -X  POST -H "Content-Type: multipart/form-data" -H "X-Exoplatform-Auth: ${jwt}" -F "file=@${files[0]}" $link)
 	if [ $resp == 200 ]; then
 		echo "[$timestamp] Uploaded recording ${files[0]} to eXo Platform" >> finalize.log
-		#rm -rf $RECORDINGS_DIR
-		#echo "[$timestamp] Removed folder $RECORDINGS_DIR" >> finalize.log
+		rm -rf $RECORDINGS_DIR
+		echo "[$timestamp] Removed folder $RECORDINGS_DIR" >> finalize.log
 	else 
 		echo "[$timestamp] Couldn't upload recording ${files[0]} to eXo Platform. HTTP_CODE: $resp" >> finalize.log
 	fi
