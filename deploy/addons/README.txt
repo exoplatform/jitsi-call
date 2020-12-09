0ec933667870c3daf9575d3d2b39bc4de5540460
Nick Riabovol <nick.riabovol@gmail.com>
2020-12-09 11:00:15 +0200

    Add scripts to automate work with git repos and branches while development

---
+++ deploy/addons/README.txt
@@ -0,0 +1,7 @@
+Scripts to automate work with git repos and branches while development
+
+Find repos and define paths in the script to work correctly. For example, /h/Work/eXo/Real/Jitsi/jitsi-addon/jitsi
+
+chmod + x $ PWD/releaseAcceptance - makes the script file executable
+
+PWD/releaseAcceptance.sh to run the script (on windows you can run through Git Bash)
---
+++ deploy/addons/resetFBsToDevelop.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# This script resets feature branches to develop
+
+GIT=`which git`
+MVN=`which mvn`
+
+# Paths to repos
+#repos[0]=/h/Work/eXo/Meeds/social                            # social
+#repos[1]=/h/Work/eXo/eXo_sources/exo-addons/chat-application # chat
+repos[2]=/h/Work/eXo/eXo_sources/exo-addons/web-conferencing # webconf
+repos[3]=/h/Work/eXo/Real/Jitsi/jitsi-addon/jitsi            # jitsi
+
+# Branches (should exist)
+developBranch="develop"
+featureBranch="feature/jitsi"
+
+function goToRepo(){
+	cd ${repos[$1]}
+	return $?
+}
+
+function goToBranch(){
+	${GIT} checkout $1
+	return $?
+}
+
+function pullBranch(){
+	${GIT} pull --rebase 
+	return $?
+}
+
+function resetBranch(){
+	${GIT} reset --hard $1
+	return $?
+}
+
+function forceBranch(){
+	${GIT} push -f
+	return $?
+}
+
+
+
+for index in ${!repos[*]}
+do
+	printf  "\n\n****************************************************\n"
+	printf  "****************************************************\n"
+	printf  "Reset feature branch to develop for ${repos[$index]}\n\n"
+
+	if ! goToRepo $index
+	then
+		echo "Error go to repo for ${repos[$index]}"
+		continue
+	fi
+	
+	if ! goToBranch $developBranch
+	then
+		echo "Error go to develop branch ($developBranch) for ${repos[$index]}"
+		continue
+	fi
+	
+	if ! pullBranch
+	then
+		echo "Error pull develop branch ($developBranch) for ${repos[$index]}"
+		continue
+	fi
+	
+	if ! goToBranch $featureBranch
+	then
+		echo "Error go to feature branch ($featureBranch) for ${repos[$index]}"
+		continue
+	fi
+	
+	if ! resetBranch $developBranch
+	then
+		echo "Error reset feature branch ($featureBranch) to develop ($developBranch) for ${repos[$index]}"
+		continue
+	fi
+	
+	if ! forceBranch
+	then
+		echo "Error force feature branch ($featureBranch) for ${repos[$index]}"
+		continue
+	fi
+	
+	printf "\nDONE"
+done
+
+printf  "\n\n"
+read -n 1 -s -r -p "Press any key to continue"
+
