4c497cc8069cdff3c3a4f4b403699460f25a5eb3
Kate Nechyporchuk <kachankatrin@gmail.com>
2020-12-17 10:11:50 +0200

    Changed icon in signin textfield; Fix color of input and icon; Made style fixes caused by new style link

--- src/main/frontend/vue-app/App.vue
+++ src/main/frontend/vue-app/App.vue
@@ -15,12 +15,26 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    input[readonly],
+    input[type="text"] {
+      background-color: transparent !important;
+      border: none;
+      margin-bottom: 0;
+      height: unset;
+      cursor: default;
+      &:focus {
+        background: transparent;
+        box-shadow: none;
+        border: none;
+        outline: none;
+      }
+    }
   }
 </style>
