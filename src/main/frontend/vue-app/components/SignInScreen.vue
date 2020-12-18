4418050ae7256ea1478f025a80537345ff2e7c16
Kate Nechyporchuk <kachankatrin@gmail.com>
2020-12-18 10:07:23 +0200

    change path for image

--- src/main/frontend/vue-app/components/SignInScreen.vue
+++ src/main/frontend/vue-app/components/SignInScreen.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
-    <img :src="'../images/logo.png'">
+    <!-- <img :src="'../images/logo.png'"> -->
+    <img src="/jitsi/images/logo.png ">
     <div id="signin-popup" ref="signinpopup">
       <div v-show="showDialog" class="background"></div>
       <v-dialog
