dfb2417ed65d3b3b601790b18bd74cde6b9c4db6
AmineFourati <afourati@exoplatform.com>
2022-01-13 17:56:42 +0100

    added a close button to the exit page for mobile users

--- src/main/frontend/vue-app/components/ExitScreen.vue
+++ src/main/frontend/vue-app/components/ExitScreen.vue
@@ -2,12 +2,23 @@
   <v-app id="exit-screen" ref="exitscreen">
     <img src="/jitsi/images/logo.png " />
     <p>This call already finished. Now you can close this window.</p>
+    <v-btn v-if="isMobile" color="primary" @click="closeWindow">close</v-btn>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "ExitScreen"
+  name: "ExitScreen" ,
+  computed:{
+    isMobile() {
+      return this.$vuetify.breakpoint.name === "xs" || this.$vuetify.breakpoint.name === "sm";
+    },
+  },
+  methods:{
+    closeWindow(){
+      window.close() ; 
+    }
+  }
 };
 </script>
  <style lang="less" scoped>
@@ -36,6 +47,7 @@ img {
 }
 </style>
 <style lang="less">
+
 input[readonly],
 input[type="text"] {
   background-color: transparent !important;
