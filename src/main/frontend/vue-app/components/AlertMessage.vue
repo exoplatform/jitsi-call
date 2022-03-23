b269be6b48e4fc1726c307a4470c96f43ec5f943
Romain Dénarié <romain.denarie@exoplatform.com>
2022-03-23 12:09:12 +0100

    Update style of invite-popup : the v-app tag was missing, so vuetify css was not loaded

---
+++ src/main/frontend/vue-app/components/AlertMessage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app id="alert-popup">
+    <v-alert
+      id="alertMessage"
+      type="success"
+      border="left"
+      colored-border
+      dismissible>
+      {{ textLink }}
+    </v-alert>
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textLink: "Link copied to clipboard",
+    };
+  }
+
+};
+</script>
+
+<style lang="less" scoped>
+@import "../../skin/less/variables.less";
+
+#alert-popup {
+  position: fixed;
+  bottom: 5%;
+  left: 10%;
+  transform: translateX(-50%);
+  font-family: Helvetica, arial, sans-serif;
+  font-weight: bold;
+  background: none;
+}
+</style>
--- src/main/frontend/vue-app/components/InvitePopup.vue
+++ src/main/frontend/vue-app/components/InvitePopup.vue
@@ -1,31 +1,43 @@
 <template>
-  <div id="invite-popup">
+  <v-app >
     <v-btn
-      id="copy-paste-icon"
+      id="icon-popup"
       :title="hoverMsg"
       icon
       @click="displayMessage"
       @click:append="displayMessage">
+      <div>
+        <input
+          ref="copyinput"
+          :value="url"
+          type="text"
+          style="opacity: 0; width: 20px; position: absolute; top: 0;"/>
+      </div>
       <div class="d-flex flex-lg-row flex-column">
         <v-icon
-          color="primary"
+          color="white"
           class="align-self-start pe-1"
           size="25">fas fa-light fa-copy
         </v-icon>
       </div>
     </v-btn>
-    <v-alert
-      v-if="alert"
-      id="alertMessage"
-      type="success"
-      border="left"
-      colored-border
-      dismissible>
-      {{ textLinkMsg }}
-    </v-alert>
-  </div>
+
+    <div id="vAlert">
+      <v-alert
+        v-if="alert"
+        type="success"
+        border="left"
+        colored-border
+        dismissible>
+        {{ textLinkMsg }}
+      </v-alert>
+    </div>
+
+  </v-app>
 </template>
+
 <script>
+
 export default {
   props: {
     // eslint-disable-next-line vue/require-default-prop
@@ -34,73 +46,78 @@ export default {
       reguired: true
     }
   },
+
   data() {
     return {
-      hoverMsg: "Copy meeting link to copyboard",
       textLink: "Link copied to clipboard",
-      active: false,
-      isClicked: false,
+      hoverMsg: "Copy meeting link to copyboard",
+      type:"",
       alert: false
     };
   },
+
   computed: {
-    style() {
-      return this.isClicked ? "display: block" : "display: none";
-    },
-    linkStyle() {
-      return this.isClicked ? "display: none" : "display: block";
-    },
     textLinkMsg(){
       return this.textLink;
     }
-
-    //   color() {
-    //     return this.isClicked ? "#46a546" : "#0376da";
-    //   },
-    //   title() {
-    //     return this.isClicked ? "Link copied to clipboard" : this.url;
-    //   }
   },
+
+  //j'essaie de faire l'appele du change position dans update pour catcher l'element après le chargement de la DOM
+  //updated(){}
+
+  mounted(){
+
+    this.changePosition();
+
+  },
+
   methods: {
-    displayMessage(message,typeAlert) {
-      this.typeAlert=this.type;
+    displayMessage() {
       this.alert = true;
       this.message=this.textLinkMsg;
       // eslint-disable-next-line no-magic-numbers
       setTimeout(() => (this.alert = false), 5000);
       this.$refs.copyinput.select();
       document.execCommand("copy");
-    }
-    /*displayMessage(message) {
-      this.isClicked = !this.isClicked;
-      this.type="success";
-      this.alert = true;
-      this.message=this.textLinkMsg;
-      window.setTimeout(() => (this.isClicked = !this.isClicked), 5000);
-      //window.setTimeout(() => this.alert = false, 5000);
-      // eslint-disable-next-line no-magic-numbers
+    },
 
-      this.$refs.copyinput.select();
-      document.execCommand("copy");
-    }*/
+
+
+    changePosition(){
+      const icon = document.getElementById("icon-popup");
+      console.log("icon:",icon);
+      const clonedIcon = icon.cloneNode(true);
+      console.log("clonedIcon:",clonedIcon);
+      /*icon.remove();*/
+      const wait = 5000;
+
+      setTimeout(this.waitInsert,wait);
+
+
+    },
+    waitInsert(){
+      const autoHide = document.getElementById("autoHide");
+      console.log("autoHide",autoHide);
+      insertAfter(clonedIcon, autoHide);
+    }
+
   }
 };
 </script>
+
 <style lang="less" scoped>
 @import "../../skin/less/variables.less";
 
-#invite-popup {
-  position: absolute;
-  bottom: 10%;
-  left: 50%;
-  transform: translateX(-50%);
-  font-family: Helvetica, arial, sans-serif;
-  font-weight: bold;
-
+#icon-popup {
+  position: fixed;
+  bottom: 94.5%;
+  left: 42.5%;
+  background: none;
 }
-
-.v-alert__border--left, .v-alert__border--right {
-  bottom: 0;
-  top: 0;
+#vAlert {
+  position: fixed;
+  bottom: 5%;
+  left: 2%;
+  background: none;
 }
 </style>
